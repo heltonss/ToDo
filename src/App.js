0f9152977f51bf353754e03dd48a3c9f59473d05
heltonss <helton.silveira@grupoltm.com.br>
2019-05-19 09:00:14 -0300

    add config reactotron

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './config/ReactotronConfig';
 import { Provider } from 'react-redux';
-
 import store from './store';
 import TodoList from './TodoList';
+
+console.tron.log('testando');
 
 const App = () => (
   <Provider store={store}>
